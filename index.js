8b8144525fe6878e545308b6905f777dfba99e4e
pk002 <32284354+pk002@users.noreply.github.com>
2018-07-03 23:54:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function (source) {
+    source = source.replace(/\/**cmft-test**/\/g, "import test from './test';");
     //source = source.replace(/<body>/g, "<body><h1>chae-loader</h1>");
-    source = " import test from './test'; " + source
+    //source = " import test from './test'; " + source
     return source;
 }
